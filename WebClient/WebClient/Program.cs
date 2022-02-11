36da1c3d0bf360d3c773f076c8888884859c758c
Alexis Martinez <alexis.martinez@yahoo.fr>
2022-02-11 17:47:42 +0100

    9.7 Les classes .NET spécialisées dans un protocole particulier de l'internet

---
+++ WebClient/WebClient/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Net;
+namespace Chap9
+{
+    public class Program
+    {
+    public static void Main(string[] args)
+        {
+            // syntaxe : [prog] Uri
+            const string syntaxe = "pg URI";
+            // nombre d'arguments
+            if (args.Length != 1)
+            {
+                Console.WriteLine(syntaxe);
+                return;
+            }
+       
+            // on note l'URI demandée
+            string stringURI = args[0];
+            
+            // vérification validité de l'URI
+            if (!stringURI.StartsWith("http://"))
+            {
+                Console.WriteLine("Indiquez une Url de la forme http://machine[:port]/document");
+                return;
+            }
+            Uri uri = null;
+            try
+            {
+                uri = new Uri(stringURI);
+            }
+            catch (Exception ex)
+            {
+                // URI incorrecte
+                Console.WriteLine("L'erreur suivante s'est produite : {0}", ex.Message);
+                return;
+            }
+            
+            try
+            {
+               // création client web
+                using (WebClient client = new WebClient())
+                {
+                    // ajout d'un entête HTTP 
+                    client.Headers.Add("user-agent", "st");
+                    using (Stream stream = client.OpenRead(uri))
+                    {
+                        using (StreamReader reader = new StreamReader(stream))
+                        {
+                            // affichage réponse du serveur web
+                            Console.WriteLine(reader.ReadToEnd());
+                            // affichage entêtes réponse du serveur
+                            Console.WriteLine("---------------------");
+                            foreach (string clé in client.ResponseHeaders.Keys)
+                            {
+                                Console.WriteLine("{0}: {1}", clé, client.ResponseHeaders[clé]);
+                            }
+                            Console.WriteLine("---------------------");
+                        }
+                    }
+                }
+            }
+            catch (WebException e1)
+            {
+                Console.WriteLine("L'exception suivante s'est produite : {0}", e1);
+            }
+            catch (Exception e2)
+            {
+                Console.WriteLine("L'exception suivante s'est produite : {0}", e2);
+            }
+        }
+    }
+}
---
+++ WebRequest/WebRequest/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Chap9
+{
+    class WebRequestResponse
+    {
+    static void Main(string[] args)
+        {
+            // syntaxe
+            const string syntaxe = "pg URI GET/HEAD";
+            
+            // nombre d'arguments
+            if (args.Length != 2)
+            {
+                Console.WriteLine(syntaxe);
+                return;
+            }
+            
+            // on note l'URI demandée
+            string stringURI = args[0];
+            string commande = args[1].ToUpper();
+            
+            // vérification validité de l'URI
+            Uri uri = null;
+            try
+            {
+                uri = new Uri(stringURI);
+            }
+            catch (Exception ex)
+            {
+                // URI incorrecte
+                Console.WriteLine("L'erreur suivante s'est produite : {0}", ex.Message);
+                return;
+            }
+            // vérification de la commande
+            if (commande != "GET" && commande != "HEAD")
+            {
+                // commande incorrecte
+                Console.WriteLine("Le second paramètre doit être GET ou HEAD");
+                return;
+            }
+            
+            try
+            {
+                // on configure la requête
+                HttpWebRequest httpWebRequest = WebRequest.Create(uri) as HttpWebRequest;
+                httpWebRequest.Method = commande;
+                httpWebRequest.Proxy = null;
+                // on l'exécute
+                HttpWebResponse httpWebResponse = httpWebRequest.GetResponse() as HttpWebResponse;
+                // résultat
+                Console.WriteLine("---------------------");
+                Console.WriteLine("Le serveur {0} a répondu : {1} {2}", httpWebResponse.ResponseUri,
+                (int)httpWebResponse.StatusCode, httpWebResponse.StatusDescription);
+                // entêtes HTTP
+                Console.WriteLine("---------------------");
+                foreach (string clé in httpWebResponse.Headers.Keys)
+                {
+                    Console.WriteLine("{0}: {1}", clé, httpWebResponse.Headers[clé]);
+                }
+                Console.WriteLine("---------------------");
+                // document
+                using (Stream stream = httpWebResponse.GetResponseStream())
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        // on affiche la réponse sur la console
+                        Console.WriteLine(reader.ReadToEnd());
+                    }
+                }
+            }
+            catch (WebException e1)
+            {
+                // on récupère la réponse
+                HttpWebResponse httpWebResponse = e1.Response as HttpWebResponse;
+                Console.WriteLine("Le serveur {0} a répondu : {1} {2}", httpWebResponse.ResponseUri,
+                (int)httpWebResponse.StatusCode, httpWebResponse.StatusDescription);
+            }
+            catch (Exception e2)
+            {
+                // on affiche l'exception
+                Console.WriteLine("L'erreur suivante s'est produite : {0}", e2.Message);
+            }
+        }
+    }
+}
